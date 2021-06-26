c843246e3a5d0453ce92e7d0ea4a5b1d06c9fb1e
Armando Soares e Silva Neto <armandosneto@outlook.com>
2021-06-26 17:40:59 -0300

    Auth implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
+import { NewRoom } from "./Pages/NewRoom";
+
+import "./styles/global.scss";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
